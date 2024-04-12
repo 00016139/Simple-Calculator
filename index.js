969a723879d2f9b7df82fc96d31a86b6712c8a3b
Muhammad Adeel Khan <160314710+00016139@users.noreply.github.com>
2024-04-12 11:15:21 +0500

    First Commit upload simple calculator
    first time upload simple calculator all files

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const Answer = await inquirer.prompt([
+    { message: "Entre First Number", type: "number", name: "First_Number" },
+    { message: "Entre Second Number", type: "number", name: "Second_Number" },
+    {
+        message: "Select one of the Operators to Perform the Action",
+        type: "list",
+        name: "Operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// make conditions
+if (Answer.Operator === "Addition") {
+    console.log(Answer.First_Number + Answer.Second_Number);
+}
+else if (Answer.Operator === "Subtraction") {
+    console.log(Answer.First_Number - Answer.Second_Number);
+}
+else if (Answer.Operator === "Multiplication") {
+    console.log(Answer.First_Number * Answer.Second_Number);
+}
+else if (Answer.Operator === "Division") {
+    console.log(Answer.First_Number / Answer.Second_Number);
+}
+else {
+    console.log("Your Operator is Invalid");
+}
